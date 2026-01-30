11a17876bdc7835dd020952e9e33e7bf6c85ebc1
jcau8 <caudwelljl2@gmail.com>
2026-01-30 18:40:08 +0000

    Added transition using an exponential gradient
    Reworked some of the main logic as well

--- SloMoMod/SloMo.cs
+++ SloMoMod/SloMo.cs
@@ -17,41 +17,97 @@ namespace SloMoMod
         private const float sloMoSpeed = 0.3f;
         // Variable to save the original time scale to
         private float baseTimeScale = Time.timeScale;
+        // Variable to track time elapsed during the transtition
+        private float elapsedTime = 0f;
+        // Length of the transition
+        private float easeDuration = 1.3f;
+        // Bool for knowing when action key is pressed
+        private bool inputRegistered = false;
+
+        public float EaseGradient(float time)
+        {
+            // Normalising time
+            float x = Mathf.Clamp01(time);
+            // Returning the gradient for the transition
+            return Mathf.Max(0f, Mathf.Pow(Mathf.Min(Mathf.Cos(Mathf.PI * x / 2f), 1 - Mathf.Abs(x)), 0.5f));
+        }
+
+        public float EaseInOut(float baseTimeScale, float sloMoSpeed, float elapsedTime, bool reverse)
+        {
+            // Creating a normalised transition timer
+            float time = Mathf.Clamp01(elapsedTime / easeDuration);
+
+            // Mapping the gradient and accentuating it
+            float gradient = 1f - Mathf.Pow((1f - EaseGradient(time)), 2f);
+
+            if (reverse)
+            {
+                // Exit transition instead of entry transition
+                return Mathf.Lerp(sloMoSpeed, baseTimeScale, gradient);
+            }
+            // Entry transition
+            return Mathf.Lerp(baseTimeScale, sloMoSpeed, gradient);
+
+        }
 
         // Get the input from controller or keyboard
         public bool SloMoInput()
         {
             // Check whether right bumper or O key are pressed
-            if (Input.GetKeyDown(KeyCode.Joystick1Button5) | Input.GetKeyDown(KeyCode.O))
+            if (Input.GetKeyDown(KeyCode.Joystick1Button5) || Input.GetKeyDown(KeyCode.O))
             {
                 return true;
             }
             return false;
         }
 
-        // Change the game's time scale
-        public void ActivateSloMo(float sloMoSpeed)
+        // Main mod function
+        public void main()
         {
-            // Set the current time scale to the original time scale
-            // This is useful if the slo-mo mode is already active
-            Time.timeScale = baseTimeScale;
+            // Check if the RB or O inputs are pressed
+            if (SloMoInput())
+            {
+                // Switch the state of slo-mo mode
+                sloMoActive = !sloMoActive;
+                // Reset elapsed time
+                elapsedTime = 0f;
+                inputRegistered = true;
+            }
 
-            // Flip the state of the bool controlling slo-mo mode
-            sloMoActive = !sloMoActive;
+            // Activate Slo-mo mode
             if (sloMoActive)
             {
-                // Change the game's time scale to the slo-mo speed
-                Time.timeScale = sloMoSpeed;
+                // Update elapsed time by real time
+                elapsedTime += Time.unscaledDeltaTime;
+                // Transition the game's time scale to the slo-mo speed
+                Time.timeScale = EaseInOut(baseTimeScale, sloMoSpeed, elapsedTime, false);
+
+                // The transition has finished, keep constant speed
+                if (elapsedTime >= easeDuration)
+                {
+                    Time.timeScale = sloMoSpeed;
+                }
             }
-            return;
+            else if (!sloMoActive && inputRegistered)
+            {
+                // The slo-mo mode is already active, use the transition and turn it off
+                // Update elapsed time by real time
+                elapsedTime += Time.unscaledDeltaTime;
+                // Transition the slo-mo time scale back to the game's original time scale
+                Time.timeScale = EaseInOut(baseTimeScale, sloMoSpeed, elapsedTime, true);
+
+                // The transition has finished, keep constant speed
+                if (elapsedTime >= easeDuration)
+                {
+                    Time.timeScale = baseTimeScale;
+                }
+            }
         }
 
         public override void OnUpdate()
         {
-            if (SloMoInput())
-            {
-                ActivateSloMo(sloMoSpeed);
-            }
+            // Run the main function
+            main();
         }
     }
 }
